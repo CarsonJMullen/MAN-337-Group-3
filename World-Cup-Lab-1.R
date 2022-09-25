ae76a7c5d420ec3d54071eeb1aa0aef2ad340d65
CarsonJMullen <96553489+CarsonJMullen@users.noreply.github.com>
2022-09-25 12:04:47 -0500

    Update World-Cup-Lab-1.R

--- World-Cup-Lab-1.R
+++ World-Cup-Lab-1.R
@@ -1,2 +1,23 @@
 library(tidyverse)
 
+#Better than Home than Away
+
+WCD <- WorldCupData %>%
+  filter(!is.na(HomeGoals)) %>%
+  filter(Date < '2022-09-19') %>%
+  mutate(HomeScoreDifferential = HomeGoals - AwayGoals) %>%
+  mutate(AwayScoreDifferential = AwayGoals - HomeGoals)
+
+WCD %>%
+  group_by(Away) %>%
+  summarise(mean = mean(HomeScoreDifferential),
+            count = n()) %>%
+  filter(count > 10) %>%
+  arrange(-mean)
+
+WCD %>%
+  group_by(Home) %>%
+  summarise(mean = mean(HomeScoreDifferential),
+            count = n()) %>%
+  filter(count > 10) %>%
+  arrange(-mean)
