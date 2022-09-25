ecbb694d122f52c267380e33a432ef79cb000375
CarsonJMullen <96553489+CarsonJMullen@users.noreply.github.com>
2022-09-25 13:11:00 -0500

    Update World-Cup-Lab-1.R

--- World-Cup-Lab-1.R
+++ World-Cup-Lab-1.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 
 #Better than Home than Away
 
@@ -21,3 +22,20 @@ WCD %>%
             count = n()) %>%
   filter(count > 10) %>%
   arrange(-mean)
+
+#Which country scored the second most goals in 2018?
+
+WCD2018Home <- WorldCupData %>%
+  filter(year(Date) == '2018') %>%
+  group_by(Home) %>%
+  summarize(Goals = sum(HomeGoals))
+  
+WCD2018Away <- WorldCupData %>%
+  filter(year(Date) == '2018') %>%
+  group_by(Away) %>%
+  summarize(Goals = sum(AwayGoals))
+
+WCD2018Home$Goals + WCD2018Away$Goals
+
+#Make an argument for which team is the most exciting to watch in the past 10 years
+
