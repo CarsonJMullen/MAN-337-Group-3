3639a7bf1439d6e1704fc3a52f8e0a7385e71cec
CarsonJMullen <96553489+CarsonJMullen@users.noreply.github.com>
2022-10-16 21:59:16 -0500

    Update NFL-Spread-Lab-3.R

--- NFL-Spread-Lab-3.R
+++ NFL-Spread-Lab-3.R
@@ -10,6 +10,7 @@ library(tidyverse)
 
 NFLGameData = NFLGameData %>%
   mutate(spread_vs_real = result - spread_line) %>%
+  mutate(favored_correct = ifelse(result * spread_line < 0, 0, 1)) %>%
   filter(!is.na(result))
 
 # Originally used mean, but decided to use median to account for the large 
@@ -17,9 +18,14 @@ NFLGameData = NFLGameData %>%
 
 NFL_By_Year <- NFLGameData %>%
   group_by(season) %>%
-  summarize(median_diff = median(abs(spread_vs_real)))
+  summarize(median_diff = median(abs(spread_vs_real)),
+            correct_percent = mean(favored_correct))
 
 ggplot(NFL_By_Year) +
   geom_line(aes(x=season, y=median_diff)) +
   ylim(0, 10)
+
+ggplot(NFL_By_Year) +
+  geom_line(aes(x=season, y=correct_percent)) +
+  ylim(0, 1)
 
