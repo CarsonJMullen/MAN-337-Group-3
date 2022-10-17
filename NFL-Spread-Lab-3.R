e3e7f24681546a91be69a0e759f76a976fed61e8
CarsonJMullen <96553489+CarsonJMullen@users.noreply.github.com>
2022-10-16 22:21:41 -0500

    Update NFL-Spread-Lab-3.R

--- NFL-Spread-Lab-3.R
+++ NFL-Spread-Lab-3.R
@@ -9,24 +9,43 @@ library(tidyverse)
 # and the advancements in advanced analytics, the accuracy of spreads would have
 # improved, but it hasn't.
 
+# Creates new variables to measure our comparisons
 NFLGameData = NFLGameData %>%
   mutate(spread_vs_real = result - spread_line) %>%
   mutate(favored_correct = ifelse(result * spread_line < 0, 0, 1)) %>%
   filter(!is.na(result))
 
-# Originally used mean, but decided to use median to account for the large 
-# outliers when a big upset occurs. 
-
+#Group by year
 NFL_By_Year <- NFLGameData %>%
   group_by(season) %>%
-  summarize(median_diff = median(abs(spread_vs_real)),
+  summarize(median_diff = mean(abs(spread_vs_real)),
             correct_percent = mean(favored_correct))
 
+# Used median rather than mean because the absolute value of the difference in
+# actual vs spread is heavily skewed to the right.
 ggplot(NFL_By_Year) +
   geom_line(aes(x=season, y=median_diff)) +
-  ylim(0, 10)
+  ylim(0, 12)
 
 ggplot(NFL_By_Year) +
   geom_line(aes(x=season, y=correct_percent)) +
+  ylim(0, 1)
+
+# This shows the percentage of games in a season that the home team covered the spread.
+# The percentage floats around 50% which makes sense because the spread is set to 
+# make gambling competitive and there should be bets on both sides of the spread.
+# This shows while odds makers may not be getting more accurate in terms of
+# difference to the spread or the correct team winning, it seems like the spread
+# is an accurate in splitting the middle. That being said, it does not appear that
+# the line has gotten tighter around 50%.
+
+NFLGameData <- NFLGameData %>% mutate(spread_correct = spread_line > result)
+
+NFLGameDataYearly <- NFLGameData %>% 
+  group_by(season) %>%
+  summarize(percent_correct = mean(spread_correct))
+
+ggplot(NFLGameDataYearly) +
+  geom_line(aes(x=season, y=percent_correct)) +
   ylim(0, 1)
 
