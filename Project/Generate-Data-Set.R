a64fb28af0c06611dc94680c5bd5b978f7408d5f
CarsonJMullen <96553489+CarsonJMullen@users.noreply.github.com>
2022-12-03 00:40:54 -0600

    Added Graph Need to Filter to 2000 Carries

--- Project/Generate-Data-Set.R
+++ Project/Generate-Data-Set.R
@@ -16,7 +16,7 @@ NFL_rushing_stats$years <- NFL_rushing_stats$season - NFL_rushing_stats$rookie_s
 NFL_rushing_stats <- NFL_rushing_stats %>%
   arrange(player_id, season, week) %>%
   group_by(player_id) %>%
-  mutate(game_number = row_number(), cum_carries = cumsum(carries), cum_rushing_yards = cumsum(rushing_yards))
+  mutate(game_number = row_number(), cum_carries = cumsum(carries), cum_rushing_yards = cumsum(rushing_yards), ypc = rushing_yards/carries)
 
 NFL_rushing_stats <- NFL_rushing_stats[,-11:-26]
 NFL_rushing_stats <- NFL_rushing_stats[NFL_rushing_stats$rookie_season > 1999,]
@@ -25,9 +25,45 @@ NFL_rushing_season_stats <- NFL_rushing_stats %>%
   group_by(player_id, season) %>%
   summarise(season_num =  mean(years),
             gp = n(), 
-            carries = sum(carries), 
+            carries = sum(carries),
             rushing_yards = sum(rushing_yards), 
             rushing_tds = sum(rushing_tds))
+
+cum_yards_season <- ave(NFL_rushing_season_stats$rushing_yards, NFL_rushing_season_stats$player_id, FUN = cumsum)
+cum_carries <- ave(NFL_rushing_season_stats$carries, NFL_rushing_season_stats$player_id, FUN = cumsum)
+
+NFL_rushing_season_stats$cum_rushing_yards <- cum_yards_season
+NFL_rushing_season_stats$cum_carries <- cum_carries
+
+average_player <- NFL_rushing_season_stats %>%
+  
+  group_by(season_num) %>%
+  summarize(cum_carries = median(cum_carries),
+            rushing_yards = median(rushing_yards))
+
+carries_vs_season <- ggplot(average_player, aes(x=season_num, y=cum_carries)) +
+  geom_line()
+rush_yds_vs_season <- ggplot(average_player, aes(x=season_num, y=rushing_yards)) +
+  geom_line()
+
+
+
+carriesColor <- "#69b3a2"
+rushingYdsColor <- rgb(0.2, 0.6, 0.9, 1)
+
+ggplot(average_player, aes(x = season_num)) +
+  geom_line(aes(y = cum_carries), color = carriesColor) + 
+  geom_line(aes(y = rushing_yards), color = rushingYdsColor) +
+  
+  scale_y_continuous(
+    name = "Cumulative Carries",
+    sec.axis = sec_axis(~.*0.4, name = "Rushing Yards")
+  ) + 
+  
+  theme(
+    axis.title.y = element_text(color = carriesColor),
+    axis.title.y.right = element_text(color = rushingYdsColor)
+  )
 
 NFL_rushing_season_stats$ypc <- NFL_rushing_season_stats$rushing_yards/NFL_rushing_season_stats$carries
 
