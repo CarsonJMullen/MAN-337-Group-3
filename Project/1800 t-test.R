a1dab2e3880e2178f704ed866277f248d03bc268
b-farias <benjamin.farias79@gmail.com>
2022-12-04 18:46:28 -0600

    Update 1800 t-test.R

--- Project/1800 t-test.R
+++ Project/1800 t-test.R
@@ -3,7 +3,7 @@ NFL_rbs_top16 <- NFL_rbs %>% slice_max(rushing_yards, n = 16)
 NFL_rbs_top16$carriesb1800 <- 0
 NFL_rbs_top16$yardsb1800 <- 0
 
-for (i in 1:nrow(NFL_rbs_top50)) {
+for (i in 1:nrow(NFL_rbs_top16)) {
   NFL_rbs_top16[i,8] = max((NFL_rushing_stats %>% filter(player_id == NFL_rbs_top16[i,1])
                             %>% filter(cum_carries < 1800))$cum_carries)
   NFL_rbs_top16[i,9] = max((NFL_rushing_stats %>% filter(player_id == NFL_rbs_top16[i,1])
