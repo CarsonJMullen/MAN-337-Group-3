296dd648cf2b8394c33be47d956fe68ebc6a6bc5
b-farias <benjamin.farias79@gmail.com>
2022-12-04 22:21:38 -0600

    Added t-test on pbp

--- Project/1800 t-test.R
+++ Project/1800 t-test.R
@@ -16,3 +16,18 @@ NFL_rbs_top16$yardsa1800 <- NFL_rbs_top16$rushing_yards - NFL_rbs_top16$yardsb18
 NFL_rbs_top16$ypca1800 <- NFL_rbs_top16$yardsa1800/NFL_rbs_top16$carriesa1800
 
 t.test(NFL_rbs_top16$ypcb1800, y = NFL_rbs_top16$ypca1800, paired = TRUE)
+
+
+
+
+
+NFL_pbp <- nflfastR::load_pbp(seasons = TRUE)
+NFL_pbp <- NFL_pbp %>% filter(season > 1999) %>% 
+  filter(rusher_player_id %in% NFL_rbs$player_id) %>%
+  filter(play_type=="run")
+NFL_rushing_pbp <- NFL_pbp %>% select(play_id, rusher_player_id, rushing_yards, epa)
+NFL_rushing_pbp <- NFL_rushing_pbp %>% filter(rusher_player_id %in% NFL_rbs$player_id[NFL_rbs$carries > 2000])
+NFL_rushing_pbp <- NFL_rushing_pbp %>% arrange(play_id) %>%
+  group_by(rusher_player_id) %>%
+  mutate(carry = row_number())
+t.test(NFL_rushing_pbp$rushing_yards[NFL_rushing_pbp$carry < 1800], NFL_rushing_pbp$rushing_yards[NFL_rushing_pbp$carry >= 1800])
